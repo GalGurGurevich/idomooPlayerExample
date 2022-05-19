e715482bb851571d98753d4d93470cd32677a654
Gal Gurevich <gal.gurevich@idomoo.com>
2022-05-19 13:44:47 +0300

    initial example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
 
-function App() {
+export default function App() {
+  const playerRef = useRef();
+  
+  function handlePlayerRef(element) {
+    playerRef.current?.dispose();
+    if (!element) {
+      playerRef.current = null;
+      return;
+    }
+
+    const player_id = element.id;
+    window.idmPlayerCreate({
+      player_id,
+      src: 'https://v.idomoo.com/3619/0000/nn62v8e1ui11363a74u17vu62428221w3e3h2mp1u3f5sn0.mp4', // can be a dynamic URL
+      size: 'HD',
+      ratio: '16:9',
+      analytics_env: 'us',
+      autoplay: false,
+      idm_logo: true,
+      remove_gif: true,
+      mute: false,
+    });
+
+    const player = playerRef.current = window[player_id];
+    
+    player.ready(() => {
+      player.on('play', () => player.requestFullscreen());
+      player.on('ended', () => player.exitFullscreen());
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div key="player" id="idm_player" ref={handlePlayerRef} />
     </div>
   );
 }
-
-export default App;
